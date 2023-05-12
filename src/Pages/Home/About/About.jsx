afa266959890868cf2dc03ca8b10dfc3ce44c511
roksana-barna <roksanabarna1803@gmail.com>
2023-05-12 23:02:37 +0600

    servicecard

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -13,9 +13,11 @@ const About = () => {
    <img className='w-1/2 absolute top-1/2 right-5 border-8 shadow-2xl border-white' src={parts}alt="" />
    </div>
     <div className='lg:w-1/2'>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
+        <h6>About Us</h6>
+
+      <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className="btn btn-warning">Get More Info</button>
     </div>
   </div>
 </div>
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services,setServices]=useState([]);
+    useEffect(()=>{
+        fetch('service.json')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
+    return (
+        <div className='mt-4'>
+           <div className='text-center'>
+           <h6 className='text-orange-600'>Service</h6>
+            <h1 className='font-bold'>Our service Area</h1>
+            <p>the majority have suffered alteration in some form, by injected humour, or randomised<br/> words which don't look even slightly believable. </p>
+            
+           </div>
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {
+                services.map(service=><ServiceCard
+                key={service._id}
+                service={service}
+                ></ServiceCard>)
+            }
+           </div>
+        </div>
+    );
+};
+
+export default Services;
