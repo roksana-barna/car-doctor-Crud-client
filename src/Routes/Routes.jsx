a1405074310fe1cf77574598bb60e8b626e7c796
roksana-barna <roksanabarna1803@gmail.com>
2023-05-13 13:20:24 +0600

    signup login

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUP from "../SignUp/SignUP";
 
 
 const router = createBrowserRouter([
@@ -11,7 +13,15 @@ const router = createBrowserRouter([
         {
             path:"/",
             element:<Home></Home>
-        }
+        },
+        {
+          path:"login",
+          element:<Login></Login>
+      },
+      {
+        path:"signup",
+        element:<SignUP></SignUP>
+    }
       ]
     },
   ]);
--- src/SignUp/SignUP.jsx
+++ src/SignUp/SignUP.jsx
@@ -1,22 +1,25 @@
 import React, { useContext } from 'react';
-import login from "../../assets/images/login/login.svg"
+import signup from "../assets/images/login/login.svg"
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../Provider/AuthProvider';
+import { AuthContext } from '../Provider/AuthProvider';
 
-const Login = () => {
-    const {signIn}=useContext(AuthContext);
-    const handleLogin=(event)=>{
+
+const SignUP = () => {
+    const {createUser}=useContext(AuthContext)
+    const handleSignup=(event)=>{
         event.preventDefault();
         const form=event.target;
         const email=form.email.value;
         const password=form.password.value;
-        console.log(email,password)
-        signIn(email,password)
-        .then(result =>{
-            const user =result.user;
-            console.log(user);
+        const confirmpassword=form.password.value;
+        const name=form.name.value;
+        console.log(name,email,password,confirmpassword)
+        createUser(email,password)
+        .then(result=>{
+            const user = result.user;
+            console.log(user)
         })
-        .catch(error=>console.log(error))
+        .then(error=>console.log(error))
 
     }
     return (
@@ -24,14 +27,20 @@ const Login = () => {
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center lg:text-left w-1/2">
                     <div className='mr-12'>
-                        <img src={login} alt="" />
+                        <img src={signup} alt="" />
 
                     </div>          </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                        
-                        <h1 className="text-3xl  text-center font-bold">Login!</h1>
-                        <form onSubmit={handleLogin}>
+                        <h1 className="text-3xl  text-center font-bold">SignUp!</h1>
+                        <form onSubmit={handleSignup}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="your name" name="name" className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -44,13 +53,19 @@ const Login = () => {
                             </label>
                             <input type="password" placeholder="password" name="password" className="input input-bordered" />
                             <label className="label">
+        
+                            </label>
+                            <span className="label-text"> Confirm Password</span>
+
+                            <input type="password" placeholder=" confirm password" name="confirmpassword" className="input input-bordered" />
+                            <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <input className='btn btn-primary' type="submit" value="Login" />
+                            <input className='btn btn-primary' type="submit" value="Sign UP" />
                         </div>
-                        <p>New to cars doctor??? please <Link className='text-orange-500' to='/signup'>Signup</Link></p>
+                        <p>Already have an account?please <Link className='text-orange-500' to='/login'>Login</Link></p>
                         </form>
                     </div>
                 </div>
@@ -59,4 +74,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUP;
