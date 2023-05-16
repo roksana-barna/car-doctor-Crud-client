5d6274193ba69d34e5e8dfae889f8a4dc338931b
roksana-barna <roksanabarna1803@gmail.com>
2023-05-16 10:58:55 +0600

    update

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const BookingRow = ({ booking,handleDelete }) => {
-  const { img, date, service, price,_id} = booking;
-  
+const BookingRow = ({ booking, handleDelete, handleConfirm }) => {
+  const { img, date, service, price, _id, status } = booking;
+
   return (
     <div>
       <tr>
         <th>
           <label>
-            <button onClick={()=>handleDelete(_id)} className="btn btn-circle btn-sm">
+            <button onClick={() => handleDelete(_id)} className="btn btn-circle btn-sm">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
             </button>
           </label>
@@ -30,7 +30,10 @@ const BookingRow = ({ booking,handleDelete }) => {
         <td>${price}</td>
 
         <th>
-          <button className="btn btn-ghost btn-xs">details</button>
+          {
+            status === 'confirm' ? <span className='font-bold text-primary'> confirmed</span> :
+
+              <button onClick={() => handleConfirm(_id)} className="btn btn-ghost btn-xs"> please Confirm</button>}
         </th>
       </tr>
 
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -10,7 +10,7 @@ const Bookings = () => {
  fetch(url)
  .then(res=>res.json())
  .then(data=>setBookings(data))
-    },[]);
+    },[url]);
     const handleDelete =id=>{
       const proceed =confirm('are you want to delete?')
       if(proceed){
@@ -29,6 +29,28 @@ const Bookings = () => {
         })
   
       }
+    }
+    const handleConfirm =id=>{
+      fetch(`http://localhost:5000/bookings/${id}`,{
+        method:'PATCH',
+        headers:{
+          'content-type':'application/json'
+        },
+        body:JSON.stringify({status:'confirm'})
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data)
+        if( data.modifiedCount >0){
+          // update state
+          const remaining = bookings.filter(booking=>booking._id !==id)
+          const updated =bookings.find(booking=>booking._id ===id)
+          updated.status ='confirm'
+          const newBookings = [updated,...remaining];
+          setBookings(newBookings)
+          
+        }
+      })
     }
     return (
         <div>
@@ -56,6 +78,7 @@ const Bookings = () => {
             key={booking._id}
             booking={booking}
             handleDelete={handleDelete}
+            handleConfirm={handleConfirm}
         ></BookingRow>)
      }
       
